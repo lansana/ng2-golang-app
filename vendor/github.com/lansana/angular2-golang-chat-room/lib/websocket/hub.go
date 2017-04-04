096bca1551a607852ede33b8db73b33082cccd41
Lansana Camara <lxc5296@gmail.com>
2017-04-04 09:06:42 -0400

    Vendor dependencies with gvt

---
+++ vendor/github.com/lansana/angular2-golang-chat-room/lib/websocket/hub.go
@@ -0,0 +1,53 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package websocket
+
+// hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	Clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.Clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.Clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.Clients, client)
+				}
+			}
+		}
+	}
+}
