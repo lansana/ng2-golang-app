008b40bd8e32807ac886db8ce1bef58d8a77fa19
Lansana Camara <lxc5296@gmail.com>
2017-04-04 11:08:46 -0400

    Add server and websocket urls

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import { port } from './port';
+
 export const environment = {
-  production: true
+  production: true,
+  websocket_url: "ws://" + window.location.hostname + port() + "/ws",
+  server_url: "http://" + window.location.hostname + port()
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,6 +3,10 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+import { port } from './port';
+
 export const environment = {
-  production: false
+  production: false,
+  websocket_url: "ws://localhost" + port() + "/ws",
+  server_url: "http://localhost" + port()
 };
