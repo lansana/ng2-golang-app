d81652ae58deb08d7d1a7809ab34f09e6c0be5f9
Lansana Camara <lxc5296@gmail.com>
2017-04-04 11:07:28 -0400

    Add function to dynamically get proper port needed

---
+++ client/src/environments/port.ts
@@ -0,0 +1,13 @@
+export function port () {
+    const port: string = window.location.port;
+    let serverPort: string;
+
+    // Running dev server, server is hosted on another port
+    if (port.indexOf("4200") > -1) {
+        serverPort = ":3030"; // Whatever port Go server is running on, or container port
+    } else {
+        serverPort = ":" + port || "80";
+    }
+
+    return serverPort;
+}
