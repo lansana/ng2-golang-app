6308d123b53890cc2eceab2a9bf79114adc7c11c
Lansana Camara <lxc5296@gmail.com>
2017-04-06 11:45:34 -0400

    Change wait to 50ms

--- api/controller/count.go
+++ api/controller/count.go
@@ -9,7 +9,8 @@ import (
 
 // Count returns a JSON response of the count of clients in the websocket hub
 func Count(c *gin.Context, hub *websocket.Hub) {
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
+
 	c.JSON(200, gin.H{
 		"success": true,
 		"count": len(hub.Clients),
